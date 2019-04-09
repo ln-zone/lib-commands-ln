7a390485680cca546941f65187bef3d7878fbf3d
cd <cd@cdpc>
2019-04-09 17:11:11 +0200

    Add "label" to addInvoiceCommand

--- src/main/java/bittech/lib/commands/ln/invoices/AddInvoiceCommand.java
+++ src/main/java/bittech/lib/commands/ln/invoices/AddInvoiceCommand.java
@@ -1,15 +1,22 @@
 package bittech.lib.commands.ln.invoices;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 import bittech.lib.protocol.Command;
 import bittech.lib.utils.Btc;
+import bittech.lib.utils.Require;
 
 public class AddInvoiceCommand extends Command<AddInvoiceRequest, AddInvoiceResponse> {
 	
 	public static AddInvoiceCommand createStub () {
-		return new AddInvoiceCommand(new Btc(), "");
+		return new AddInvoiceCommand(new Btc(), "inv" + RandomStringUtils.random(12, false, true), "");
 	}
 	
-	public AddInvoiceCommand(Btc value, String memo) {
-		this.request = new AddInvoiceRequest(value, memo);
+	public AddInvoiceCommand(Btc value, String label, String memo) {
+		this.request = new AddInvoiceRequest(value, label, memo);
+	}
+	
+	public AddInvoiceCommand(AddInvoiceRequest request) {
+		this.request = Require.notNull(request, "request");
 	}
 }
--- src/main/java/bittech/lib/commands/ln/invoices/AddInvoiceRequest.java
+++ src/main/java/bittech/lib/commands/ln/invoices/AddInvoiceRequest.java
@@ -7,10 +7,12 @@ import bittech.lib.utils.Require;
 public class AddInvoiceRequest implements Request {
 	
 	public Btc value;
+	public String label;
 	public String memo;
 	
-	public AddInvoiceRequest(Btc value, String memo) {
+	public AddInvoiceRequest(Btc value, String label, String memo) {
 		this.value = Require.notNull(value, "value");
+		this.label = Require.notNull(label, "label");
 		this.memo = Require.notNull(memo, "memo");
 	}
 
